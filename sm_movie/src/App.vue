eb7f0b429d1c5fa38dc32844e9f0881ae53d98e8
gifeminum <gifeminum@gmail.com>
2019-05-05 17:15:53 +0700

    create app.vue

--- sm_movie/src/App.vue
+++ sm_movie/src/App.vue
@@ -1,11 +1,30 @@
 <template>
-  <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <router-link to="/selectPosition">
-          <a class="nav-link">Select</a>
-    </router-link>
-    <router-view/>
-  </div>
+<div id="app">
+  <div class="ui inverted segment">
+    <div class="ui inverted secondary menu">
+      <span class="item">
+        <img class="ui image" src="./assets/sm.png" height="60px" width="60px" > 
+      </span>
+      <a href="#/" class="item">
+        <i class="home icon"></i>
+        Home
+      </a>
+      <a href="#/movie" class="item">
+        <i class="film icon"></i>
+        Movies
+      </a>
+      <a class="item">
+        <i class="address book outline icon"></i>
+        About us
+      </a>
+      <a class="right floated item">
+        <i class="user outline icon"></i>
+        Login / Register
+      </a>
+    </div>
+  </div> 
+  <router-view/>  
+</div>
 </template>
 
 <script>
@@ -15,12 +34,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body  {
+        background-image: url("./assets/bg.jpg") !important;
+        background-size: cover !important;
+        }
 </style>
---
+++ sm_movie/src/components/movie.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="ui center container">
+    <div class="ui segment">
+    </div>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
