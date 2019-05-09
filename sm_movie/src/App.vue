c094cbf7ca91410aaf89b13ca3d7772cbdd0845b
gifeminum <gifeminum@gmail.com>
2019-05-09 21:04:09 +0700

    แก้  App.vue

--- sm_movie/src/App.vue
+++ sm_movie/src/App.vue
@@ -43,12 +43,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body  {
+      background-image: url("./assets/bg.jpg") !important;
+      background-size: cover !important;
+      }
 </style>
